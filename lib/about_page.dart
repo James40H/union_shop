2d20ebefb847932ba72517e34303af5d2d331040
James  Hind <jameshind2005@gmail.com>
2025-11-21 17:26:30 +0000

    add main text

--- lib/about_page.dart
+++ lib/about_page.dart
@@ -154,6 +154,24 @@ class AboutPage extends StatelessWidget {
                           ),
                         ),
                         const SizedBox(height: 16),
+                        const Text(
+                          '''Welcome to the Union Shop!
+
+We’re dedicated to giving you the very best University branded products, with a range of clothing and merchandise available to shop all year round! We even offer an exclusive personalisation service!
+
+All online purchases are available for delivery or instore collection!
+
+We hope you enjoy our products as much as we enjoy offering them to you. If you have any questions or comments, please don’t hesitate to contact us at hello@upsu.net.
+
+Happy shopping!
+
+The Union Shop & Reception Team''',
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.black87,
+                            height: 1.4,
+                          ),
+                        ),
                       ],
                     ),
                   ),
