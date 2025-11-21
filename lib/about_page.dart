b188058e5ae6b5636751471da32ad9f9327b1732
James  Hind <jameshind2005@gmail.com>
2025-11-21 16:28:31 +0000

    add about page and impot to main

---
+++ lib/about_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AboutPage extends StatelessWidget {
+  const AboutPage({super.key});
+
+  void navigateToHome(BuildContext context) {
+    Navigator.pushNamedAndRemoveUntil(context, '/', (route) => false);
+  }
+
+  void placeholderCallbackForButtons() {
+    // This is the event handler for buttons that don't work yet
+  }
+}
